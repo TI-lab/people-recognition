d9c67bf805a9e25cbd9a87c266e1bfc47960315e
Alexander Freeman <alexander.freeman.nl@gmail.com>
2017-10-03 14:34:37 +0200

    Added serial triggering

--- frontend/main.py
+++ frontend/main.py
@@ -1,5 +1,10 @@
 import numpy as np
+import serial
 import cv2
+import threading  # :C
+
+
+global g_image  # :C
 
 
 def draw_face(image, face):
@@ -13,13 +18,32 @@ def cut_face(image, face):
     return image[y:y + h, x:x + w]
 
 
+serial_port = serial.Serial('/dev/ttyUSB0')
+
+
+def handle_data(data):
+    print(data)
+    cv2.imshow('hi', g_image)
+
+
+def read_from_port(ser):
+    while True:
+        print("test")
+        reading = ser.readline().decode()
+        handle_data(reading)
+
+
+thread = threading.Thread(target=read_from_port, args=(serial_port,))
+thread.start()
+
+
 face_cascade = cv2.CascadeClassifier('frontalface_cascade.xml')
 cap = cv2.VideoCapture(0)
 
 while(cap.isOpened()):
     ret, image = cap.read()
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5, minSize=(30, 30))
     for face in faces:
         draw_face(image, face)
 
@@ -27,7 +51,7 @@ while(cap.isOpened()):
 
     cut_faces = [cut_face(image, face) for face in faces]
     if len(cut_faces) != 0:
-        cv2.imshow('cut', cut_faces[0])
+        g_image = cut_faces[0]
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
